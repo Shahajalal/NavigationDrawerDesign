93a614b7c5f36905250a9d697a77768d9ff898de
Shahajalal <35290545+Shahajalal@users.noreply.github.com>
2019-01-12 14:19:42 +0600

    f

---
+++ app/src/main/java/com/example/shahajalal/navigationdrawerdesign/StoreFragment.java
@@ -0,0 +1,35 @@
+package com.example.shahajalal.navigationdrawerdesign;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.shahajalal.navigationdrawerdesign.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class StoreFragment extends Fragment {
+
+
+    public StoreFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.content_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getActivity().setTitle("Store");
+    }
+}
